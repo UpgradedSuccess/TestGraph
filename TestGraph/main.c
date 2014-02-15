89da7c5a1fb1b4ee4ce088cc057de8c80042ced5
Myrkul <pro4myrkul@gmail.com>
2014-02-15 20:47:27 +0100

    Comentarios & limpieza
    He añadido comentarios simples, pa entenderme. También he acabado por
    completo con la estructura "evento", ahora se guarda todo en la
    estructura "mapp".

--- TestGraph/main.c
+++ TestGraph/main.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+﻿#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <conio.h>
@@ -8,50 +8,40 @@
 
 typedef struct
 {
-	int pos[MAX][2];
-	char name[MAX][30];
-	char text[MAX][100];
-} evento;
-
-typedef struct
-{
 	char map[MAX][MAX];
 	char desc[MAX][MAX][30];
 	char text[MAX][MAX][100];
 	int dest[MAX][MAX][3];
 } mapeado;
 
-int world(int[9][2], char, mapeado[MAX], int[2], evento[MAX], int[MAX], int*);
-int lectura(int[9][2], mapeado*, int[MAX], evento*, int*, int[10][2][3], int*);
-int display(int[9][2], int[MAX], mapeado*, evento*);
+int world(int[9][2], char, mapeado[MAX], int[2], int[MAX], int*);
+int lectura(int[9][2], mapeado*, int[MAX], int*, int[10][2][3], int*);
+int display(int[9][2], int[MAX], mapeado*);
 
 int main()
 {
 	int posicion[9][2], numevent[MAX], numlinks=0, nummapas=0, link[10][2][3];
-	evento *eventos = NULL;
 	mapeado *mapp = NULL;
 
-	mapp = (mapeado*)calloc(MAX, sizeof(mapeado));
-	eventos = (evento*)calloc(MAX, sizeof(evento));
-	lectura(posicion, mapp, numevent, eventos, &numlinks, link, &nummapas);
+	mapp = (mapeado*)calloc(MAX, sizeof(mapeado));	
+	lectura(posicion, mapp, numevent, &numlinks, link, &nummapas);
 	mapp = (mapeado*)realloc(mapp, (nummapas+1)* sizeof(mapeado));
-	eventos = (evento*)realloc(eventos, (nummapas+1)* sizeof(evento));
-	display(posicion, numevent, mapp, eventos);
+	display(posicion, numevent, mapp);
 }
 
-int world(int posicion[9][2], char mov, mapeado mapp[MAX], int pj[2], evento eventos[MAX], int numevent[MAX], int *mapactual)
+int world(int posicion[9][2], char mov, mapeado mapp[MAX], int pj[2], int numevent[MAX], int *mapactual)
 {
-	int k, aux[3];
+	int aux[3];
 
 	switch (mov)
 	{
 	case 'w':
 		if (mapp[*mapactual].map[pj[1] - 1][pj[0]] == '#')
-			return 0;
+			return 0; //El personaje no se mueve
 		else if (mapp[*mapactual].map[pj[1] - 1][pj[0]] == '-' || mapp[*mapactual].map[pj[1] - 1][pj[0]] == 'E')
-			return 1;
+			return 1; //El personaje se mueve
 		else
-			return 1;
+			return 1; //El personaje se mueve
 	case 'a':
 		if (mapp[*mapactual].map[pj[1]][pj[0] - 1] == '#')
 			return 0;
@@ -76,20 +66,11 @@ int world(int posicion[9][2], char mov, mapeado mapp[MAX], int pj[2], evento eve
 	case 'e':
 		if (mapp[*mapactual].map[pj[1]][pj[0]] == '!')
 		{
-			for (k = 0; k < numevent[*mapactual]; k++)
-			{
-				if (pj[0] == eventos[*mapactual].pos[k][0])
-				{
-					if (pj[1] == eventos[*mapactual].pos[k][1])
-					{
-						printf("\n\n\n");
-						printf("%s:\n", eventos[*mapactual].name[k]);
-						printf("-%s\n", eventos[*mapactual].text[k]);
-						getch();
-						return 1;
-					}
-				}
-			}
+			printf("\n\n\n");
+			printf("%s:\n", mapp[*mapactual].desc[pj[1]][pj[0]]);
+			printf("-%s\n", mapp[*mapactual].text[pj[1]][pj[0]]);
+			getch();
+			return 1;
 		}
 		else if (mapp[*mapactual].map[pj[1]][pj[0]] == 'E')
 		{
@@ -104,15 +85,17 @@ int world(int posicion[9][2], char mov, mapeado mapp[MAX], int pj[2], evento eve
 	}
 }
 
-int lectura(int posicion[9][2], mapeado *mapp, int numevent[MAX], evento *eventos, int *numlinks, int link[10][2][3], int *nummapas)
+int lectura(int posicion[9][2], mapeado *mapp, int numevent[MAX], int *numlinks, int link[10][2][3], int *nummapas)
 {
-	int k, i;
+	int k, i, posEvento[2];
 	char caracter, nombremapa[10], nombrevento[15], aux1[3][5] = { { '0', '0', '0', '0', '0' }, { '.', 't', 'x', 't', '\0' }, { 'm', 'a', 'p', 'a', '\0' } }, aux2[3][7] = { { '0', '0', '0', '0', '0', '0', '0' }, { '.', 't', 'x', 't', '\0', '\0', '\0' }, { 'e', 'v', 'e', 'n', 't', 's', '\0' } };
 	FILE* mapa[10];
 	FILE* events[10];
 	FILE* links;
 
 	links = fopen("links.txt", "rt");
+
+	//####Lectura del archivo de links####//
 	while (feof(links) == 0)
 	{
 		caracter = fgetc(links);
@@ -125,6 +108,7 @@ int lectura(int posicion[9][2], mapeado *mapp, int numevent[MAX], evento *evento
 		fscanf(links, "%d %d %d ", &link[k][0][0], &link[k][0][1], &link[k][0][2]);
 		fscanf(links, "%d %d %d\n", &link[k][1][0], &link[k][1][1], &link[k][1][2]);
 	}
+	//####################################//
 	do
 	{
 		strcpy(nombremapa, aux1[2]);
@@ -135,7 +119,7 @@ int lectura(int posicion[9][2], mapeado *mapp, int numevent[MAX], evento *evento
 		if (mapa[(*nummapas)] == NULL)
 			break;
 		(*nummapas)++;
-	} while (1);
+	} while (1); //Bucle de apertura de archivos de mapas.
 
 	for (k = 0; k < (*nummapas);k++)
 	{
@@ -144,7 +128,7 @@ int lectura(int posicion[9][2], mapeado *mapp, int numevent[MAX], evento *evento
 		strcat(nombrevento, aux2[0]);
 		strcat(nombrevento, aux2[1]);
 		events[k] = fopen(nombrevento, "rt");
-	}
+	} //Bucle de apertura de archivos de eventos.
 	
 	for (k = 0; k < (*nummapas); k++)
 	{
@@ -152,7 +136,9 @@ int lectura(int posicion[9][2], mapeado *mapp, int numevent[MAX], evento *evento
 		posicion[k][1] = 0;
 		while (feof(mapa[k]) == 0)
 		{
-			mapp[k].map[posicion[k][1]][posicion[k][0]] = fgetc(mapa[k]);
+			mapp[k].map[posicion[k][1]][posicion[k][0]] = fgetc(mapa[k]);	//Almacenamiento del mapa.
+
+			//###Almacenamiento de descripciones del mapa##//
 			if (mapp[k].map[posicion[k][1]][posicion[k][0]] == '-')
 				strcpy(mapp[k].desc[posicion[k][1]][posicion[k][0]], "Llanos");
 			else if (mapp[k].map[posicion[k][1]][posicion[k][0]] == '#')
@@ -160,15 +146,17 @@ int lectura(int posicion[9][2], mapeado *mapp, int numevent[MAX], evento *evento
 			else if (mapp[k].map[posicion[k][1]][posicion[k][0]] == 'E')
 			{
 				strcpy(mapp[k].desc[posicion[k][1]][posicion[k][0]], "Puerta");
-				for (i = 0; i < (*numlinks); i++)
+
+				for (i = 0; i < (*numlinks); i++) //Almacenamiento de links de puertas.
 				{
-					if (link[i][0][0] == k && link[i][0][1] == posicion[k][1] && link[i][0][2] == posicion[k][0])
+					//##Emparejamiento de cada link con su puerta##//
+					if (link[i][0][0] == k && link[i][0][1] == posicion[k][1] && link[i][0][2] == posicion[k][0]) //Búsqueda de emparejamientos en los links de origen
 					{
 						mapp[k].dest[posicion[k][1]][posicion[k][0]][0] = link[i][1][0];
 						mapp[k].dest[posicion[k][1]][posicion[k][0]][1] = link[i][1][1];
 						mapp[k].dest[posicion[k][1]][posicion[k][0]][2] = link[i][1][2];
 					}
-					else if (link[i][1][0] == k && link[i][1][1] == posicion[k][1] && link[i][1][2] == posicion[k][0])
+					else if (link[i][1][0] == k && link[i][1][1] == posicion[k][1] && link[i][1][2] == posicion[k][0])	//Búsqueda de emparejamientos en los links de destino
 					{
 						mapp[k].dest[posicion[k][1]][posicion[k][0]][0] = link[i][0][0];
 						mapp[k].dest[posicion[k][1]][posicion[k][0]][1] = link[i][0][1];
@@ -178,7 +166,7 @@ int lectura(int posicion[9][2], mapeado *mapp, int numevent[MAX], evento *evento
 			}
 			if (mapp[k].map[posicion[k][1]][posicion[k][0]] == '\n')
 			{
-				posicion[k][1]++;
+				posicion[k][1]++;	//El vector "posicion" se utiliza para ir apuntando a todos los tiles del mapa.
 				posicion[k][0] = 0;
 				continue;
 			}
@@ -186,9 +174,11 @@ int lectura(int posicion[9][2], mapeado *mapp, int numevent[MAX], evento *evento
 		}
 		fclose(mapa[k]);
 	}
+	//##Se añade \0 al final de cada línea para que no se solapen los saltos de línea (Tú déjalo como está)##//
 	for (i = 0; i < (*nummapas); i++)
 		for (k = 0; k < posicion[i][1]; k++)
 			mapp[i].map[k][posicion[i][0]-1] = 0;
+	//##Lectura y almacenamiento de los archivos de eventos##//
 	for (i = 0; i < (*nummapas); i++)
 	{
 		numevent[i] = 0;
@@ -202,23 +192,18 @@ int lectura(int posicion[9][2], mapeado *mapp, int numevent[MAX], evento *evento
 		rewind(events[i]);
 		for (k = 0; k < numevent[i]; k++)
 		{
-			fscanf(events[i], "%d %d\n", &eventos[i].pos[k][0], &eventos[i].pos[k][1]);
-			fscanf(events[i], "%[^\n]s", eventos[i].name[k]);
+			fscanf(events[i], "%d %d\n", &posEvento[0], &posEvento[1]);
+			fscanf(events[i], "%[^\n]s", mapp[i].desc[posEvento[1]][posEvento[0]]);
 			caracter = fgetc(events[i]);
-			fscanf(events[i], "%[^\n]s", eventos[i].text[k]);
+			fscanf(events[i], "%[^\n]s", mapp[i].text[posEvento[1]][posEvento[0]]);
+			mapp[i].map[posEvento[1]][posEvento[0]] = '!';
 		}
 		fclose(events[i]);
-		for (k = 0; k < numevent[i]; k++)
-		{
-			mapp[i].map[eventos[i].pos[k][1]][eventos[i].pos[k][0]] = '!';
-			strcpy(mapp[i].desc[eventos[i].pos[k][1]][eventos[i].pos[k][0]], eventos[i].name[k]);
-			strcpy(mapp[i].text[eventos[i].pos[k][1]][eventos[i].pos[k][0]], eventos[i].text[k]);
-		}
 	}
 	return 0;
 }
 
-int display(int posicion[9][2], int numevent[MAX], mapeado *mapp, evento *eventos)
+int display(int posicion[9][2], int numevent[MAX], mapeado *mapp)
 {
 	int k, i, coll, pj[2] = { 3, 3 }, mapactual = 0, cont1, cont2;
 	char mov, auxmap[MAX][MAX];
@@ -229,23 +214,24 @@ int display(int posicion[9][2], int numevent[MAX], mapeado *mapp, evento *evento
 		cont2 = 0;
 		k = 0;
 		i = 0;
+		//##Bucle usado para guardar una parte del mapa (definida por VISIONX y VISIONY) en un mapa auxiliar para poder mostrarlo correctamente.
 		for (i = (pj[1] - VISIONY); i < pj[1] + (VISIONY + 1); i++)
 		{
 			for (k = (pj[0] - VISIONX); k < pj[0] + (VISIONX + 2); k++)
 			{
-				if (k == pj[0] && i == pj[1])
+				if (k == pj[0] && i == pj[1]) //Posición del personaje
 				{
 					auxmap[cont1][cont2] = '*';
 					cont2++;
 					continue;
 				}
-				if (k == (pj[0] + (VISIONX + 1)))
+				if (k == (pj[0] + (VISIONX + 1))) //Frontera del mapa
 					auxmap[cont1][cont2] = '\0';
 				else
 				{
-					if (k >= posicion[mapactual][0] || k<0 || i>posicion[mapactual][1] || i < 0)
+					if (k >= posicion[mapactual][0] || k<0 || i>posicion[mapactual][1] || i < 0) //Exterior del mapa
 						continue;
-					else
+					else //Resto del mapa
 						auxmap[cont1][cont2] = mapp[mapactual].map[i][k];
 				}
 				cont2++;
@@ -253,15 +239,15 @@ int display(int posicion[9][2], int numevent[MAX], mapeado *mapp, evento *evento
 			cont2 = 0;
 			cont1++;
 		}
-		for (k = 0; k < VISIONX+1; k++)
-		{
+		for (k = 0; k < VISIONX+1; k++) //Bucle para mostrar el mapa
 			puts(auxmap[k]);
-		}
 		printf("\nMapa: %d\n%s", mapactual+1, mapp[mapactual].desc[pj[1]][pj[0]]);
+
+		//##Movimiento##//
 		switch (mov = getch())
 		{
 		case 'w':
-			coll = world(posicion, mov, mapp, pj, eventos, numevent, &mapactual);
+			coll = world(posicion, mov, mapp, pj, numevent, &mapactual); //Colision
 			if (coll == 0)
 				break;
 			else if (coll == 1)
@@ -271,7 +257,7 @@ int display(int posicion[9][2], int numevent[MAX], mapeado *mapp, evento *evento
 				break;
 			}
 		case 'a':
-			coll = world(posicion, mov, mapp, pj, eventos, numevent, &mapactual);
+			coll = world(posicion, mov, mapp, pj, numevent, &mapactual); //Colision
 			if (coll == 0)
 				break;
 			else if (coll == 1)
@@ -281,7 +267,7 @@ int display(int posicion[9][2], int numevent[MAX], mapeado *mapp, evento *evento
 				break;
 			}
 		case 's':
-			coll = world(posicion, mov, mapp, pj, eventos, numevent, &mapactual);
+			coll = world(posicion, mov, mapp, pj, numevent, &mapactual); //Colision
 			if (coll == 0)
 				break;
 			else if (coll == 1)
@@ -291,7 +277,7 @@ int display(int posicion[9][2], int numevent[MAX], mapeado *mapp, evento *evento
 				break;
 			}
 		case 'd':
-			coll = world(posicion, mov, mapp, pj, eventos, numevent, &mapactual);
+			coll = world(posicion, mov, mapp, pj, numevent, &mapactual); //Colision
 			if (coll == 0)
 				break;
 			else if (coll == 1)
@@ -301,7 +287,7 @@ int display(int posicion[9][2], int numevent[MAX], mapeado *mapp, evento *evento
 				break;
 			}
 		case 'e':
-			world(posicion, mov, mapp, pj, eventos, numevent, &mapactual);
+			world(posicion, mov, mapp, pj,numevent, &mapactual); //Colision
 			break;
 		}
 		system("cls");
