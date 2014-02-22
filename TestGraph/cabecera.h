79c0deba618e682b7cb9f8d11f764742229c861e
Myrkul <pro4myrkul@gmail.com>
2014-02-22 17:10:48 +0100

    Cabecera actualizada
    Ahora se puede escoger el punto de inicio del personaje

--- TestGraph/cabecera.h
+++ TestGraph/cabecera.h
@@ -1,17 +1,35 @@
-#include <stdio.h>
+﻿#include <stdio.h>
 #define MAX 30
-#define VISIONX 4
+#define VISIONX 4 // A ser posible, VISIONY será VISIONX-2. Más que nada para que el mapa sea cuadrado
 #define VISIONY 2
+#define INICIOPJX 3
+#define INICIOPJY 3
 
 typedef struct
 {
 	char map[MAX][MAX];
-	char desc[MAX][MAX][30];
+	char desc[MAX][MAX][15];
 	char text[MAX][MAX][MAX][50];
-	int dest[MAX][MAX][3];
+	int dest[MAX][MAX][MAX];
 	int numTexto[MAX][MAX];
 } mapeado;
 
-void colisiones(mapeado[], int[2], int*, int[][2]);
-void lectura(int[][2], mapeado*, int*);
-void display(int[][2], mapeado*);
+typedef struct
+{
+	int map;
+	int X;
+	int Y;
+}link;
+
+typedef struct
+{
+	int X;
+	int Y;
+}pos;
+
+extern mapeado *mapp;
+extern pos *tamMapa;
+
+void colisiones(pos*, int*, pos[]);
+void lectura(pos[]);
+void display(pos[]);
