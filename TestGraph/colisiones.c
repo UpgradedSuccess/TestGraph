298b65c9de2f0598ffb7eaf5fd4115e77c2c0b2c
Myrkul <pro4myrkul@gmail.com>
2014-02-22 17:10:16 +0100

    Cambios en las variables auxiliares
    Archivos actualizados para que las variables auxiliares sean más
    intuitivas

--- TestGraph/colisiones.c
+++ TestGraph/colisiones.c
@@ -2,80 +2,84 @@
 #include <windows.h>
 #include <conio.h>
 
-void colisiones(mapeado mapp[], int pj[2], int *mapactual, int tamMapa[MAX][2])
+mapeado *mapp;
+pos *tamMapa;
+
+void colisiones(pos *pj, int *mapactual)
 {
-	int aux[3], k, i;
+	int k, i;
 	char mov;
+	link aux;
 
 	switch (mov = getch())
 	{
 	case 'w':
-		if (mapp[*mapactual].map[pj[1] - 1][pj[0]] == '#')
+		if (mapp[*mapactual].map[pj->Y - 1][pj->X] == '#')
 		{
 			printf("\a");
 			return;
 		}
-		else if (mapp[*mapactual].map[pj[1] - 1][pj[0]] == '-' || mapp[*mapactual].map[pj[1] - 1][pj[0]] == 'E' || mapp[*mapactual].map[pj[1] - 1][pj[0]] == '!')
+		else if (mapp[*mapactual].map[pj->Y - 1][pj->X] == '-' || mapp[*mapactual].map[pj->Y - 1][pj->X] == 'E' || mapp[*mapactual].map[pj->Y - 1][pj->X] == '!')
 		{
-			if (pj[1] != 0)
-				pj[1]--;
-			return; //El personaje se mueve
+			if (pj->Y != 0)
+				pj->Y--;
+			return;
 		}
 		else
-			return; //El personaje se mueve
+			return;
 	case 'a':
-		if (mapp[*mapactual].map[pj[1]][pj[0] - 1] == '#')
+		if (mapp[*mapactual].map[pj->Y][pj->X - 1] == '#')
 		{
 			printf("\a");
 			return;
 		}
-		else if (mapp[*mapactual].map[pj[1]][pj[0] - 1] == '-' || mapp[*mapactual].map[pj[1]][pj[0] - 1] == 'E' || mapp[*mapactual].map[pj[1]][pj[0] - 1] == '!')
+		else if (mapp[*mapactual].map[pj->Y][pj->X - 1] == '-' || mapp[*mapactual].map[pj->Y][pj->X - 1] == 'E' || mapp[*mapactual].map[pj->Y][pj->X - 1] == '!')
 		{
-			if (pj[0] != 0)
-				pj[0]--;
+			if (pj->X != 0)
+				pj->X--;
 			return;
 		}
 		else
 			return;
 	case 's':
-		if (mapp[*mapactual].map[pj[1] + 1][pj[0]] == '#')
+		if (mapp[*mapactual].map[pj->Y + 1][pj->X] == '#')
 		{
 			printf("\a");
 			return;
 		}
-		else if (mapp[*mapactual].map[pj[1] + 1][pj[0]] == '-' || mapp[*mapactual].map[pj[1] + 1][pj[0]] == 'E' || mapp[*mapactual].map[pj[1] + 1][pj[0]] == '!')
+		else if (mapp[*mapactual].map[pj->Y + 1][pj->X] == '-' || mapp[*mapactual].map[pj->Y + 1][pj->X] == 'E' || mapp[*mapactual].map[pj->Y + 1][pj->X] == '!')
 		{
-			if (pj[1] != tamMapa[*mapactual][1])
-				pj[1]++;
+			if (pj->Y != tamMapa[*mapactual].Y)
+				pj->Y++;
 			return;
 		}
 		else
 			return;
 	case 'd':
-		if (mapp[*mapactual].map[pj[1]][pj[0] + 1] == '#')
+		if (mapp[*mapactual].map[pj->Y][pj->X + 1] == '#')
 		{
 			printf("\a");
 			return;
 		}
-		else if (mapp[*mapactual].map[pj[1]][pj[0] + 1] == '-' || mapp[*mapactual].map[pj[1]][pj[0] + 1] == 'E' || mapp[*mapactual].map[pj[1]][pj[0] + 1] == '!')
+		else if (mapp[*mapactual].map[pj->Y][pj->X + 1] == '-' || mapp[*mapactual].map[pj->Y][pj->X + 1] == 'E' || mapp[*mapactual].map[pj->Y][pj->X + 1] == '!')
 		{
-			if (pj[0] != tamMapa[*mapactual][0] - 2)
-				pj[0]++;
+			if (pj->X != tamMapa[*mapactual].X - 2)
+				pj->X++;
 			return;
 		}
 		else
 			return;
 	case 'e':
-		if (mapp[*mapactual].map[pj[1]][pj[0]] == '!')
+		if (mapp[*mapactual].map[pj->Y][pj->X] == '!')
 		{
 			printf("\n\n\n");
-			printf("%s:\n", mapp[*mapactual].desc[pj[1]][pj[0]]);
+			printf("%s:\n", mapp[*mapactual].desc[pj->Y][pj->X]);
 			printf("-");
-			for (i = 0; i < mapp[*mapactual].numTexto[pj[1]][pj[0]]; i++)
+			for (i = 0; i < mapp[*mapactual].numTexto[pj->Y][pj->X]; i++)
 			{
-				for (k = 0; k < strlen(mapp[*mapactual].text[i][pj[1]][pj[0]]); k++)
+				for (k = 0; k < strlen(mapp[*mapactual].text[pj->Y][pj->X][i]); k++)
 				{
-					printf("%c", mapp[*mapactual].text[i][pj[1]][pj[0]][k]);
+					printf("%c", mapp[*mapactual].text[pj->Y][pj->X][i][k]);
 					Sleep(50);
 				}
 				printf("\n ");
@@ -83,14 +87,14 @@ void colisiones(mapeado mapp[], int pj[2], int *mapactual, int tamMapa[MAX][2])
 			}
 			return;
 		}
-		else if (mapp[*mapactual].map[pj[1]][pj[0]] == 'E')
+		else if (mapp[*mapactual].map[pj->Y][pj->X] == 'E')
 		{
-			aux[0] = mapp[*mapactual].dest[pj[1]][pj[0]][0];
-			aux[1] = mapp[*mapactual].dest[pj[1]][pj[0]][1];
-			aux[2] = mapp[*mapactual].dest[pj[1]][pj[0]][2];
-			*mapactual = aux[0];
-			pj[1] = aux[1];
-			pj[0] = aux[2];
+			aux.map = mapp[*mapactual].dest[pj->Y][pj->X][0];
+			aux.Y = mapp[*mapactual].dest[pj->Y][pj->X][1];
+			aux.X = mapp[*mapactual].dest[pj->Y][pj->X][2];
+			*mapactual = aux.map;
+			pj->Y = aux.Y;
+			pj->X = aux.X;
 		}
 		return;
 	}
--- TestGraph/display.c
+++ TestGraph/display.c
@@ -1,45 +1,51 @@
 ﻿#include "cabecera.h"
 
-void display(int tamMapa[][2], mapeado *mapp)
-{
-	int k, i, pj[2] = { 3, 3 }, mapactual = 0, cont1, cont2;
-	char auxmap[MAX][MAX];
+mapeado *mapp;
+pos *tamMapa;
 
+void display()
+{
+	int k, i, mapactual = 0;
+	char auxmap[MAX][MAX];
+	pos pj, aux;
+
+	pj.X = INICIOPJX;
+	pj.Y = INICIOPJY;
 	do
 	{
-		cont1 = 0;
-		cont2 = 0;
+		aux.Y = 0;
+		aux.X = 0;
 		k = 0;
 		i = 0;
 		//##Bucle usado para guardar una parte del mapa (definida por VISIONX y VISIONY) en un mapa auxiliar para poder mostrarlo correctamente.
-		for (i = (pj[1] - VISIONY); i < pj[1] + (VISIONY + 1); i++)
+		for (i = (pj.Y - VISIONY); i < pj.Y + (VISIONY + 1); i++)
 		{
-			for (k = (pj[0] - VISIONX); k < pj[0] + (VISIONX + 2); k++)
+			for (k = (pj.X - VISIONX); k < pj.X + (VISIONX + 2); k++)
 			{
-				if (k == pj[0] && i == pj[1]) //Posición del personaje
+				if (k == pj.X && i == pj.Y) //Posición del personaje
 				{
-					auxmap[cont1][cont2] = '*';
-					cont2++;
+					auxmap[aux.Y][aux.X] = '*';
+					aux.X++;
 					continue;
 				}
-				if (k == (pj[0] + (VISIONX + 1))) //Frontera del mapa
-					auxmap[cont1][cont2] = '\0';
+				if (k == (pj.X + (VISIONX + 1))) //Frontera del mapa
+					auxmap[aux.Y][aux.X] = '\0';
 				else
 				{
-					if (k >= tamMapa[mapactual][0] || k<0 || i>tamMapa[mapactual][1] || i < 0) //Exterior del mapa
+					if (k >= tamMapa[mapactual].X || k<0 || i>tamMapa[mapactual].Y || i < 0) //Exterior del mapa
 						continue;
 					else //Resto del mapa
-						auxmap[cont1][cont2] = mapp[mapactual].map[i][k];
+						auxmap[aux.Y][aux.X] = mapp[mapactual].map[i][k];
 				}
-				cont2++;
+				aux.X++;
 			}
-			cont2 = 0;
-			cont1++;
+			aux.X = 0;
+			aux.Y++;
 		}
 		for (k = 0; k < VISIONX + 1; k++) //Bucle para mostrar el mapa
 			puts(auxmap[k]);
-		printf("\nMapa: %d\n%s", mapactual + 1, mapp[mapactual].desc[pj[1]][pj[0]]);
-		colisiones(mapp, pj, &mapactual, tamMapa); //Colision
+		printf("\nMapa: %d\n%s", mapactual + 1, mapp[mapactual].desc[pj.Y][pj.X]);
+		colisiones(&pj, &mapactual, tamMapa); //Colision
 		system("cls");
 	} while (1);
 }
