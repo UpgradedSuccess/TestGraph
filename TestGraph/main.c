5893257201ec8521b3af6ea9b04030bba177c75e
Myrkul <pro4myrkul@gmail.com>
2014-03-20 00:45:58 +0100

    Nueva estructura
    Ahora las funciones se van llamando desde main. El bug de la acumulación
    de memoria ha vuelto, es posible que esté relacionado con esto.

--- TestGraph/main.c
+++ TestGraph/main.c
@@ -2,8 +2,9 @@
 
 int main()
 {
-	int numevento = 0, mapactual, numlink, numitems = 0;
+	short numevento = 0, mapactual, numlink, numitems = 0, mov, bat, k;
 	char **map;
+	bool updatemap = true;
 	STRUCTpos VISION, tamMapa = { 0 };
 	STRUCTgraph graph;
 	STRUCTcontroles controles;
@@ -14,10 +15,37 @@ int main()
 
 	srand(time(0));
 	setlocale(LC_CTYPE, "Spanish");
-
 	mapactual = 0;
 	inicializacion(&numitems, &VISION, &graph, &controles, &personaje, &items, &mapactual);
+	intromenu(&items, controles, &personaje, &mapactual);
 	lectura(&numevento, &mapactual, &numlink, &tamMapa, &map, &evento, &puertas);
-	instrucciones();
-	display(numitems, numevento, &mapactual, &numlink, &VISION, &tamMapa, &graph, &controles, &personaje, &map, &evento, items, &puertas);
+	do
+	{
+		bat = rand() % 25;
+		if (bat == 0)
+			batalla(numitems, controles, &personaje, items);
+		display(evento, VISION, tamMapa, graph, &personaje, map, updatemap, numevento, mapactual);
+		mov = movimiento(numevento, &updatemap, map, &controles, &personaje, evento);
+		switch (mov)
+		{
+		case 1:
+			if ((busquedaPuerta(&mapactual, &numlink, &personaje, puertas)) == 1)
+			{
+				for (k = 0; k <= tamMapa.Y; k++)
+					free(map[k]);
+				free(evento);
+				updatemap = true;
+				lectura(&numevento, &mapactual, &numlink, &tamMapa, &map, &evento, &puertas);
+			}
+			break;
+		case 2:
+			pjmenu(controles, &personaje, items, mapactual);
+			break;
+		case 3:
+			adminmenu(&VISION, &graph, &controles, &personaje);
+			break;
+		default:
+			break;
+		}
+	} while (1);
 }
